fab95e659b61bdc40f05c71a13794440697d72fc
Vital <vitalmago.job@gmail.com>
2020-04-29 14:44:09 +0600

    Add new file Logger.java, for debug tracking with encapsulation.

--- app/src/main/java/com/example/sandbox/MainActivity.java
+++ app/src/main/java/com/example/sandbox/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.sandbox;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Logger.debug(TAG, "OnCreate");
     }
 }
