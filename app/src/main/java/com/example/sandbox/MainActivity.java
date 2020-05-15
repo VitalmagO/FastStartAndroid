b8a729b351a1cacc894c36d38e4c7e8822b6ceab
Vital <vitalmago.job@gmail.com>
2020-05-15 13:02:08 +0600

    Alert Dialog

--- app/src/main/java/com/example/sandbox/MainActivity.java
+++ app/src/main/java/com/example/sandbox/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.sandbox;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,8 +13,6 @@ public class MainActivity extends AppCompatActivity {
 
     private final String TAG = getClass().getSimpleName();
     private Button mButton1;
-    private Button mButton2;
-    private Button mButton3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,40 +20,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Logger.debug(TAG, "OnCreate");
 
-        mButton1 = findViewById(R.id.btn_1);
-        mButton2 = findViewById(R.id.btn_2);
-        mButton3 = findViewById(R.id.btn_3);
-
+        mButton1 =  findViewById(R.id.btn_1);
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                switch (view.getId()) {
-                    case R.id.btn_1:
-                        switchFragment("one", R.color.red);
-                        break;
-                    case R.id.btn_2:
-                        switchFragment("two", R.color.green);
-                        break;
-                    case R.id.btn_3:
-                        switchFragment("three", R.color.blue);
-                        break;
-                }
+            public void onClick(View v) {
+                showInfo();
             }
         };
         mButton1.setOnClickListener(onClickListener);
-        mButton2.setOnClickListener(onClickListener);
-        mButton3.setOnClickListener(onClickListener);
     }
-    void switchFragment(String name, int color) {
-        Fragment fragment;
-        fragment = getSupportFragmentManager().findFragmentByTag(name);
-        if (fragment == null) {
-            fragment = SampleFragment.newInstance(name, ContextCompat.getColor(this, color));
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, fragment, name)
-                    .commit();
-        } else {
-            Toast.makeText(this, "Не меняем", Toast.LENGTH_SHORT).show();
-        }
+    private void showInfo() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Info")
+                .setMessage("London is the capital of Great Britain")
+                .setPositiveButton("true", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "You are absolutely right", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("false", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Wrong", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .show();
     }
 }
