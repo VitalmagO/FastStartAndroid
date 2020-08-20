522953c5716fa3ab263841775e37c84268d69b0b
Vital <vitalmago.job@gmail.com>
2020-08-20 13:25:48 +0600

    Миграция логики AuthActivity в SingleFragment

---
+++ app/src/main/java/com/example/sandbox/SingleFragmentActivity.java
@@ -0,0 +1,24 @@
+package com.example.sandbox;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_single_fragment);
+        if (savedInstanceState == null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainer, getFragment())
+                    .commit();
+        }
+    }
+
+    protected abstract Fragment getFragment();
+}
